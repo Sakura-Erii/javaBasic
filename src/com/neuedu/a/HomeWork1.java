8499891167f3fdac2ea3b77899fdf70ef4ec21ad
sakuraerii <2321646599@qq.com>
2019-05-23 20:05:41 +0800

    作业一

--- src/com/neuedu/a/HomeWork1.java
+++ src/com/neuedu/a/HomeWork1.java
@@ -2,7 +2,7 @@ package com.neuedu.a;
 
 public class HomeWork1 {
     public static void main(String[] args) {
-        int a=147;
+        int a=125;
         int b=a%10;
         int c=(a%100-b)/10;
         int d=(a%1000-b-c*10)/100;
